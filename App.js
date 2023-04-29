c86c065986eb57ac9aa4130ff86b1dd1fc49947c
gabr-silva <gabrielvieira.s0801@gmail.com>
2023-04-29 20:05:08 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -8,47 +8,9 @@ import { useFonts } from 'expo-font';
 import ConfirmarDados from './src/login/confirmarDados';
 import Formulario from './src/login/Formulario'
 import Menu from './src/janelas/menu';
-import styles from './estilo';
+import Janela2 from './src/janelas/janela2';
 
 const Stack = createNativeStackNavigator();
-
-
-//Janela 2
-const Janela2 = ({navigation}) => {
-  return (
-    <View style={styles.container}>
-      <Text>Segunda Janela</Text>
-      <TouchableOpacity
-        onPress={()=>{navigation.navigate('Formulario')}}
-        style={styles.botao}
-        >
-        <Text>ACESSAR FORMULÁRIO</Text>
-      </TouchableOpacity>
-    </View>
-  )
-}
-
-const Janela3 = ({navigation}) => {
-  return(
-    <View style={styles.container}>
-      <Text>Cadastro</Text>
-      <TouchableOpacity
-        style={styles.botao}
-        onPress={()=>{navigation.push('Janela4')}}
-        >
-          <Text style={{color: 'white'}}>Confirmar Dados</Text>
-        </TouchableOpacity>
-    </View>
-  )
-}
-
-const Janela4 = () =>{
-  return(
-    <View style={styles.container}>
-      <Text>Confirmação de dados</Text>
-    </View>
-  )
-}
 
 export default function App() {
   return (
@@ -61,7 +23,6 @@ export default function App() {
             navigationBarColor: '#ea337c'}}/>
           <Stack.Screen name="Janela2" component={Janela2}/>
           <Stack.Screen name="Formulario" component={Formulario}/>
-          <Stack.Screen name="Janela4" component={Janela4}/>
           <Stack.Screen name="ConfirmarDados" component={ConfirmarDados}/>
       </Stack.Navigator>
     </NavigationContainer>
---
+++ src/janelas/janela2.js
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from 'react';
+import {View, Text, TextInput, TouchableOpacity, ScrollView} from 'react-native';
+import styles from '../../estilo';
+
+const Janela2 = () =>{
+
+}
+
+export default Janela2;
